1148ac1ffb4d28f76832467a2cd5b6484701669e
aviola98 <viola1@usp.br>
2021-08-16 15:02:00 -0300

    commit

---
+++ 2_integers_print_bigger.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+  int main () {
+
+      int number1, number2;
+      scanf ("%d", &number1);
+      scanf("%d",&number2);
+
+      if (number1 > number2)
+      printf("%d", number1);
+      else
+      printf("%d", number2);
+
+      return 0;
+      
+  }
