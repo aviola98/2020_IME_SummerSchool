1cf0c1f58908b8601b6a0a0e1e2aac78dddb3b46
aviola98 <viola1@usp.br>
2021-08-16 14:57:41 -0300

    first commit

---
+++ division_bill.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main (){ 
+    
+    int carlos_valor, andre_valor;
+    float felipe_valor, total_valor;
+    float carlos_valorf, andre_valorf;
+    
+    scanf("%f", &total_valor);
+    
+    carlos_valor = total_valor/3;
+    andre_valor = carlos_valor;
+    felipe_valor = total_valor - (carlos_valor + andre_valor);
+    
+    carlos_valorf = (float)carlos_valor;
+    andre_valorf = (float)andre_valor;
+    
+    
+    printf("Andre: %.2f, Carlos: %.2f e Felipe: %.2f", andre_valorf, carlos_valorf, felipe_valor);
+    
+    return 0;
+    
+} 
